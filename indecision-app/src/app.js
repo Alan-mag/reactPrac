060d36e5c07d278ad9dc20b80f3cfcf92d9fc903
Alan Magelssen <alanmagelssen@gmail.com>
2017-10-20 17:35:12 -0700

    react add

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,7 @@
-// import './utils';
-import subtract, { square, add } from './utils'; // defaults not in curly braces
-import isSenior, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running!');
-
-// console.log(square(4));
-// console.log(add(400, 11));
-// console.log(subtract(100, 25));
-
-console.log(isAdult(20));
-console.log(canDrink(20));
-console.log(isSenior(77));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
+// import validator from 'validator';
+// console.log(validator.isEmail('duke@gmail.com'));
