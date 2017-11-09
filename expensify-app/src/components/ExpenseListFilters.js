f0170c1a03200c5f3bec206487e0c00c01eef00c
Alan Magelssen <alanmagelssen@gmail.com>
2017-11-09 11:33:54 -0800

    addExpense update

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -8,7 +8,9 @@ const ExpenseListFilters = (props) => (
     <input type="text" value={props.filters.text} onChange={(e) => {
       props.dispatch(setTextFilter(e.target.value));
     }} />
-    <select onChange={(e) => {
+    <select 
+      value={props.filters.sortBy}
+      onChange={(e) => {
       if (e.target.value === 'date') {
         props.dispatch(sortByDate())
       } else if (e.target.value === 'amount') {
